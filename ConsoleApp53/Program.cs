b09f0b79320eb50289ac99557e92371d80137eb2
IthronRandir <frtopo@yandex.by>
2023-04-21 17:12:44 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp53/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void SumOfN(int a1, int d, int n)
+        {
+            int Sn = (2 * a1 + d * (n - 1)) / 2 * n;
+            
+            Console.WriteLine(Sn);
+        }
+        static void Main(string[] args)
+        {
+            int a1 = 2;
+            int d = 3;
+            int n = 5;
+            SumOfN(a1, d, n);
+        }
+    }
+}
